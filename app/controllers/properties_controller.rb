2ce3845738a0abc915c24b99fd603db3063c5115
Arvind Mehra <your.bloomberg490@gmail.com>
2014-04-16 10:52:50 +0530

    make form and submit it

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,40 @@
+class PropertiesController < ApplicationController
+
+	def index
+		@properties = Property.all
+	end
+
+	def new
+		@properties = Property.new
+	end
+
+	def show
+		@property = Property.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def create
+		@property = Property.new(property_params) 
+    if @property.save
+      redirect_to properties_path
+    else
+      render 'new'
+    end
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+
+	private
+
+    def property_params
+      params.require(:property).permit(:project, :area, :price,
+                                   :city,:sizes,:contact)
+    end
+
+end
