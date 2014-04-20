7bd6acb9b4440ef2b21390916d266f9f0d4729b3
Arvind Mehra <your.bloomberg490@gmail.com>
2014-04-21 00:48:51 +0530

    show table to specific user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,9 +10,9 @@ class WelcomeController < ApplicationController
       params[:search][:min_price]||= ""
       params[:search][:max_price]||= ""
       params[:search][:to_do]||= ""
-      @properties = Property.search_data(params[:search])
+      @properties = current_user.properties.search_data(params[:search])
     else
-      @properties = Property.all.paginate(:page => params[:page], :per_page => 10)
+      @properties = current_user.properties.paginate(:page => params[:page], :per_page => 10)
     end
   end
 
@@ -25,7 +25,7 @@ class WelcomeController < ApplicationController
     params[:search][:min_price]||= ""
     params[:search][:max_price]||= ""
     params[:search][:to_do]||= ""
-    @properties = Property.search_data(params[:search]).paginate(:page => params[:page], :per_page => 10)
+    @properties = current_user.properties.search_data(params[:search]).paginate(:page => params[:page], :per_page => 10)
     redirected_to welcome_search_path(params.merge(params[:search]))
   end
 
