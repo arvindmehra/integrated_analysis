73920574aacc31357b79b0619303e627d81d2807
Arvind Mehra <your.bloomberg490@gmail.com>
2014-04-20 16:14:45 +0530

    fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,17 @@ class WelcomeController < ApplicationController
 
   layout 'new_layout', :only => [:login]
 
-  def index
-    params[:search][:min_sqrt]||= ""
-    params[:search][:max_sqrt]||= ""
-    params[:search][:min_price]||= ""
-    params[:search][:max_price]||= ""
-    params[:search][:to_do]||= ""
-    @properties = Property.search_data(params[:search]).paginate(:page => params[:page], :per_page => 10)
+  def index 
+    if params[:search].present?
+      params[:search][:min_sqrt]||= ""
+      params[:search][:max_sqrt]||= ""
+      params[:search][:min_price]||= ""
+      params[:search][:max_price]||= ""
+      params[:search][:to_do]||= ""
+      @properties = Property.search_data(params[:search]).paginate(:page => params[:page], :per_page => 10)
+    else
+      @properties = Property.all.paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
   def login
