97399d5db34126fcc16fffed26c63e720cb91161
amitkumarsuroliya <amitkumarsuroliya@gmail.com>
2014-04-20 15:32:24 +0530

    search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,19 @@ class WelcomeController < ApplicationController
   layout 'new_layout', :only => [:login]
 
   def index
- 	  @properties = Property.all.paginate(:page => params[:page], :per_page => 10)
+    params[:search][:min_sqrt]||= ""
+    params[:search][:max_sqrt]||= ""
+    params[:search][:min_price]||= ""
+    params[:search][:max_price]||= ""
+    params[:search][:to_do]||= ""
+    @properties = Property.search_data(params[:search]).paginate(:page => params[:page], :per_page => 10)
   end
 
   def login
   end
 
   def search
-  	redirected_to welcome_search_path
+    redirected_to welcome_search_path
   end
 
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,7 @@
 class Property < ActiveRecord::Base
 
+	scope :size_between,lambda {|from,to| {:conditions => ["Property.size between ? and ?",from,to]}}
+	scope :price_between,lambda {|from,to| {:conditions => ["price between ? and ?",from,to]}}
 	TODO_TYPE = {
     "Rent" => "0",
     "Sale" => "1",
@@ -16,5 +18,29 @@ class Property < ActiveRecord::Base
   BUILDING_AMENITIES = ["Parking garage","Lobby attendant","Gym","Common kitchen","Common bike storage","Common showers","Onsite parking","Bikes allowed","Dog friendly","Common bathrooms","Onsite security guard"]
 
   self.per_page = 10
+
+  def self.search_data(search)
+  	lc = ActiveSupport::OrderedHash.new
+    lc[:min_sqrt] = []
+    lc[:max_sqrt] = []
+    lc[:min_price] = []
+    lc[:max_price] = []
+    lc[:to_do] =[]
+    scope = Property.all
+    if search && !search[:min_sqrt].blank? && !search[:max_sqrt].blank?
+    	debugger
+      scope = scope.where{|x| x.size >= search[:min_sqrt] && x.size <= search[:max_sqrt]}
+      scope = scope.instance_values.values.last
+    end
+    if search && !search[:min_price].blank? && !search[:max_price].blank?
+    	scope = scope.where{|x| x.size >= search[:min_price] && x.size <= search[:max_price]}
+    	scope = scope.instance_values.values.last
+    end
+    if search && !search[:to_do].blank?
+      scope = scope.where{|x| x.to_do == search[:to_do]}
+      scope = scope.instance_values.values.last
+    end
+    scope
+  end
 
 end
