0d93b338e377dbb519cd38ccba615892914803f5
amitkumarsuroliya <amitkumarsuroliya@gmail.com>
2014-04-21 22:27:09 +0530

    second commit to change layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def collapser
+		link_to("#",class: "btn btn-navbar", data: {toggle: "collapse", target: ".nav-collapse"}) do
+			("<span class='icon-bar'>"*3).html_safe
+		end
+	end
+
+
 end
